f5fd0f3d810f06963a43e65cc27bdb171690eb7e
Joshua Lee <josh.lee134@hotmail.com>
2023-02-17 00:00:22 -0800

    fix delete(key) method in maps.rb

--- W3D5/map.rb
+++ W3D5/map.rb
@@ -3,7 +3,6 @@ class Map
   
   def initialize
     @map = Array.new {Array.new(2)}
-    @index = 0
   end
 
   def set(key, value)
@@ -19,7 +18,11 @@ class Map
   end
 
   def delete(key)
-    @map.delete(key)
+    (0...@map.length).each do |i|
+      if @map[i][0] == key
+        @map.delete_at(i)
+      end
+    end
   end
 
   def show
@@ -33,6 +36,11 @@ if $PROGRAM_NAME == __FILE__
   map.set("jack",4)
   map.set("queen",3)
   map.set("king",3)
-  map.delete("king")
+  map.set("king",5)
+  p map
+  p map.delete("king")
+  p map
+  p map.set(2,3)
+  p map.delete(2)
   p map
 end
