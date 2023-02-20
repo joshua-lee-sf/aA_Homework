ab0fea263bf9b2a8a6e3da71dc4aab29fefab274
Joshua Lee <josh.lee134@hotmail.com>
2023-02-20 13:21:12 -0800

    update W4D1

--- W4D1/graphnode.rb
+++ W4D1/graphnode.rb
@@ -8,17 +8,24 @@ class GraphNode
     @neighbors = []
   end
 
+end
+
+
   def bfs(starting_node, target_value)
     visited = Set.new
     nodes_queue = [self.value]
 
     until nodes_queue.empty?
       node = node_queue.shift
+      unless visited.include?(node)
+        return node.val if node.val == target_value
+        visitied.add(node)
+        nodes_queue += node.neighbors
+      end
     end
 
+    return nil
   end
-
-end
 
 if $PROGRAM_NAME == __FILE__
   a = GraphNode.new('a')
