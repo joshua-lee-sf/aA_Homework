3515bd009081a5b155ec80294c0182e159d5b37a
Joshua Lee <josh.lee134@hotmail.com>
2023-03-01 00:08:55 -0800

    update old homework

--- W5D1/lru_cache.rb
+++ W5D1/lru_cache.rb
@@ -16,17 +16,24 @@ class LRUCache
     # if the size is not at the max_size, add the element into the list
     # if the cache is at the max_size, delete the oldest item and add the newest item in.
     # if the item is already in the cache, delete that item and add it back into the list
-    if self.count < @max_size && !@underlying_array.include?(el)
-      @underlying_array << el
-    elsif self.count == @max_size && !@underlying_array.include?(el)
+    if self.count == @max_size && !@underlying_array.include?(el)
       @underlying_array.shift
       @underlying_array << el
-    elsif self.count == @max_size && @underlying_array.include?(el)
+    elsif self.count == @max_size || @underlying_array.include?(el)
       @underlying_array.delete(el)
       @underlying_array << el
     else
       @underlying_array << el
     end
+    # if @underlying_array.include?(el)
+    #   @underlying_array.delete(el)
+    #   @underlying_array << el
+    # elsif count >= @max_size
+    #   @underlying_array.shift
+    #   @underlying_array << el
+    # else
+    #   @underlying_array << el
+    # end
   end
 
   def show
@@ -41,19 +48,19 @@ end
 
 johnny_cache = LRUCache.new(4)
 
-johnny_cache.add("I walk the line") #["I walk the line"]
-johnny_cache.add(5) #["I walk the line", 5]
+p johnny_cache.add("I walk the line") #["I walk the line"]
+p johnny_cache.add(5) #["I walk the line", 5]
 
-p johnny_cache.count # => returns 2
+johnny_cache.count # => returns 2
 
-johnny_cache.add([1,2,3]) #["I walk the line", 5, [1,2,3]]
-johnny_cache.add(5) #["I walk the line", [1,2,3], 5]
-johnny_cache.add(-5) #["I walk the line", [1,2,3], 5, -5]
-johnny_cache.add({a: 1, b: 2, c: 3})  #[[1,2,3], 5, -5, {a: 1, b: 2, c: 3}]
-johnny_cache.add([1,2,3,4]) #[5, -5, {a: 1, b: 2, c: 3}, [1,2,3,4]]
-johnny_cache.add("I walk the line") #[ -5, {a: 1, b: 2, c: 3}, [1,2,3,4], "I walk the line"]
-johnny_cache.add(:ring_of_fire)  #[{a: 1, b: 2, c: 3}, [1,2,3,4], "I walk the line", :ring_of_fire]
-johnny_cache.add("I walk the line") #[{a: 1, b: 2, c: 3}, [1,2,3,4], :ring_of_fire, "I walk the line"]
+p johnny_cache.add([1,2,3]) #["I walk the line", 5, [1,2,3]]
+p johnny_cache.add(5) #["I walk the line", [1,2,3], 5]
+p johnny_cache.add(-5) #["I walk the line", [1,2,3], 5, -5]
+p johnny_cache.add({a: 1, b: 2, c: 3})  #[[1,2,3], 5, -5, {a: 1, b: 2, c: 3}]
+p johnny_cache.add([1,2,3,4]) #[5, -5, {a: 1, b: 2, c: 3}, [1,2,3,4]]
+p johnny_cache.add("I walk the line") #[ -5, {a: 1, b: 2, c: 3}, [1,2,3,4], "I walk the line"]
+p johnny_cache.add(:ring_of_fire)  #[{a: 1, b: 2, c: 3}, [1,2,3,4], "I walk the line", :ring_of_fire]
+p johnny_cache.add("I walk the line") #[{a: 1, b: 2, c: 3}, [1,2,3,4], :ring_of_fire, "I walk the line"]
 johnny_cache.add({a: 1, b: 2, c: 3}) #[[1,2,3,4], :ring_of_fire, "I walk the line", {a: 1, b: 2, c: 3}]
 
 johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
